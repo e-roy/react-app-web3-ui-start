4ad92c0f8671914497e73faef768d4cc619cafb3
e-roy <ericroupe@gmail.com>
2022-01-15 13:55:57 -0500

    initial start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ConnectWallet } from "@web3-ui/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ConnectWallet />
+    </div>
+  );
+}
+
+export default App;
